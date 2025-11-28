25a8655438d1c203e2e2945fb626b3d4703c628a
Deeptadeep Choudhury <choudhury.rio@gmail.com>
2025-11-28 21:04:07 +0530

    first commit

---
+++ starter/src/test/java/com/order_service_devops/starter/StarterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.order_service_devops.starter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StarterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
